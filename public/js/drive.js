a831a1caadd2b12726046f50ecc1b26668707e50
Koen <iamkodec@gmail.com>
2017-12-29 12:44:30 +0100

    added checkbox functionality

---
+++ public/js/drive.js
@@ -0,0 +1,26 @@
+window.onload=function(){
+    var checkbox = document.querySelector("input[name=driveSwitch]");
+    checkbox.addEventListener('change', setDrive);
+}
+function setDrive() {
+    if(this.checked) {
+        axios.post('http://things.ubidots.com/api/v1.6/devices/surveillancecar/drive/values/?token=A1E-yz1uifC28k1uUWlvVQNUI40TCNXB6y',
+        {
+            "value": 1
+        })
+        .then(res => {
+            console.log('uploaded');
+        })
+        .catch(err => {console.log(err)});
+    } else {
+        // Checkbox is not checked..
+        axios.post('http://things.ubidots.com/api/v1.6/devices/surveillancecar/drive/values/?token=A1E-yz1uifC28k1uUWlvVQNUI40TCNXB6y',
+        {
+            "value": 0
+        })
+        .then(res => {
+            console.log('uploaded');
+        })
+        .catch(err => {console.log(err)});
+    }
+}
